d63961c1c19a3ceafb3b255c7177a059154204cf
roberto-na <54071118+roberto-na@users.noreply.github.com>
2023-03-11 20:34:53 -0800

    Add files via upload

---
+++ Binary_Bcd_test.vhd
@@ -0,0 +1,56 @@
+--library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+			
+ENTITY Bin_BCD_TB IS
+END Bin_BCD_TB;
+ 
+ARCHITECTURE behavior OF Bin_BCD_TB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Bin_BCD
+    PORT(
+         Bin : IN  std_logic_vector(11 downto 0);
+         BCD : OUT  std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Bin : std_logic_vector(11 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal BCD : std_logic_vector(15 downto 0);
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Bin_BCD PORT MAP (
+          Bin => Bin,
+          BCD => BCD
+        );
+ 
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		
+      -- insert stimulus here 
+		Bin <= conv_std_logic_vector(183,12);--"10110111";
+		      wait for 100 ns;	
+		Bin <= conv_std_logic_vector(129,12);--"10000001";
+		      wait for 100 ns;	
+		Bin <= conv_std_logic_vector(57,12);--"00111001";
+				wait for 100 ns;	
+		Bin <= conv_std_logic_vector(123,12);--"01111011";
+		      wait for 100 ns;	
+
+
+      wait;
+   end process;
+
---
+++ Converter.vhd
@@ -0,0 +1,22 @@
+-- Code downloaded from: https://en.wikipedia.org/wiki/Double_dabble
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+use work.Conv_Package.all;
+
+
+
+entity Converter is
+    Port ( Bin : in  STD_LOGIC_VECTOR (11 downto 0);
+           BCD : out  STD_LOGIC_VECTOR (15 downto 0));
+end Bin_BCD;
+
+architecture Behavioral of Converter is
+
+begin
+	
+	BCD <= Double_Dabble(Bin);
+
+end Behavioral;
